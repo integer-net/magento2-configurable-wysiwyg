3bfcaa811d55cc3689cbeb8cef90f47f817905e9
Andreas von Studnitz <avs@avs-webentwicklung.de>
2020-09-11 12:46:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# ![/>](./dev/favicon.png) integer_net Magento 2 Module Template
+
+This is a GitHub template.
+
+**Create your module now with the "use this template" button:**
+
+[![Use this template](dev/use-this-template.png)](https://github.com/integer-net/magento2-module-template/generate)
+
+After that, clone it on your machine and run `/dev/init` for automatic configuration. For more information, see [dev/README.md](dev/README.md)
+
+<div align="center"><img src="https://www.integer-net.de/wp-content/uploads/2012/11/firmenprofil.jpg" alt="Waschbär Approved Module" /></div>
+
+---
+
+<!-- TEMPLATE -->
+
+# :module-namespace_:module-name Magento Module
+<div align="center">
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Build Status][ico-travis]][link-travis]
+![Supported Magento Versions][ico-compatibility]
+
+[![Coverage Status][ico-scrutinizer]][link-scrutinizer]
+[![Quality Score][ico-code-quality]][link-code-quality]
+[![Maintainability][ico-maintainability]][link-maintainability]
+</div>
+
+---
+
+:description
+
+## Installation
+
+1. Install it into your Magento 2 project with composer:
+    ```
+    composer require :vendor/:package
+    ```
+
+2. Enable module
+    ```
+    bin/magento setup:upgrade
+    ```
+
+## Configuration
+
+## Usage
+
+## Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## Testing
+
+### Unit Tests
+
+```
+vendor/bin/phpunit tests/unit
+```
+
+### Magento Integration Tests
+
+0. Configure test database in `dev/tests/integration/etc/install-config-mysql.php`. [Read more in the Magento docs.](https://devdocs.magento.com/guides/v2.4/test/integration/integration_test_execution.html) 
+
+1. Copy `tests/integration/phpunit.xml.dist` from the package to `dev/tests/integration/phpunit.xml` in your Magento installation.
+
+2. In that directory, run
+    ``` bash
+    ../../../vendor/bin/phpunit
+    ```
+
+
+## Security
+
+If you discover any security related issues, please email :author-email instead of using the issue tracker.
+
+## Credits
+
+- [:author-name][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/:vendor/:package.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/:vendor/:package/master.svg?style=flat-square
+[ico-scrutinizer]: https://img.shields.io/scrutinizer/coverage/g/:vendor/:package?style=flat-square
+[ico-code-quality]: https://img.shields.io/scrutinizer/g/:vendor/:package.svg?style=flat-square
+[ico-maintainability]: https://img.shields.io/codeclimate/maintainability/:vendor/:package?style=flat-square
+[ico-compatibility]: https://img.shields.io/badge/magento-:version-badge-brightgreen.svg?logo=magento&longCache=true&style=flat-square
+
+[link-packagist]: https://packagist.org/packages/:vendor/:package
+[link-travis]: https://travis-ci.org/:vendor/:package
+[link-scrutinizer]: https://scrutinizer-ci.com/g/:vendor/:package/code-structure
+[link-code-quality]: https://scrutinizer-ci.com/g/:vendor/:package
+[link-maintainability]: https://codeclimate.com/github/:vendor/:package
+[link-author]: https://github.com/:author-github
+[link-contributors]: ../../contributors
---
+++ dev/src/Config.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace IntegerNet\ModuleTemplate;
+
+use function array_merge as merge;
+
+/**
+ * These functions contain configuration for the init script
+ */
+class Config
+{
+    /**
+     * @return string[] relative paths for files where placeholders will be replaced
+     */
+    public static function getFilesToUpdate(): array
+    {
+        return [
+            'auth.json',
+            'composer.json',
+            '.travis.yml',
+            '.travis/before_script.sh',
+            'LICENSE',
+            'README.md',
+            'CONTRIBUTING.md',
+            'registration.php',
+            'src/ExampleClass.php',
+            'src/etc/module.xml',
+            'tests/integration/phpunit.xml.dist',
+            'tests/integration/ExampleTest.php',
+            'tests/unit/ExampleTest.php',
+            'tests/unit/phpunit.xml.dist',
+            'tests/src/ExampleTestDataBuilder.php',
+        ];
+    }
+
+    /**
+     * @return string[] Associative array with placeholders as keys and default values as values
+     */
+    public static function getDefaultVariables(): array
+    {
+        // Get author name and email from Git if possible
+        $gitUserName = trim(shell_exec('git config --get user.name'));
+        $gitUserEmail = trim(shell_exec('git config --get user.email'));
+
+        // Get vendor and package from Github URI if possible (matches HTTPS and SSH)
+        preg_match('#github\.com[:/]([^/]+)/(.*)\.git#', shell_exec('git remote get-url origin'), $matches);
+        $vendor = $matches[1] ?? 'acme-inc';
+        $package = $matches[2] ?? 'magento2-awesome-module';
+
+        // Guess company, namespace and module name based on vendor and package
+        $company = ucwords(str_replace('-', ' ', $vendor));
+        $moduleNamespace = str_replace(' ', '', $company);
+        $commonPrefixes = self::getCommonModulePrefixes($moduleNamespace);
+        $moduleName = str_replace(merge([' '], $commonPrefixes), '', ucwords(str_replace(['-', '_'], ' ', $package)));
+
+        return [
+            ':magento-repo-public-key'  => '',
+            ':magento-repo-private-key' => '',
+            ':vendor'                   => $vendor,
+            ':package'                  => $package,
+            ':description'              => 'This module is awesome!',
+            ':author-name'              => $gitUserName ?: 'John Doe',
+            ':author-email'             => $gitUserEmail ?: 'john.doe@example.com',
+            ':author-github'            => $vendor,
+            ':module-namespace'         => $moduleNamespace,
+            ':module-name'              => $moduleName,
+            ':company'                  => $company,
+            ':year'                     => (string)date('Y'),
+        ];
+    }
+
+    /**
+     * Exact PHP requirements are already handled by magento/framework itself, but making it explicit allows the module
+     * author to tighten the constraints, e.g. make the module work with Magento 2.3 but still require at least PHP 7.3.
+     *
+     * Also, the supported PHP versions are used by {@see TravisJob::getConfiguration()} to generate Travis jobs.
+     *
+     * The following placeholder values will be generated automatically based on choosen compatible versions:
+     *
+     * @return string[][][] Associative array with Magento versions and dependencies for composer
+     * @see Initialize::askMagentoCompatibility()
+     *
+     *  :php-constraint         Composer version constraint for PHP
+     *  :framework-constraint   Composer version constraint for Magento framework
+     *  :version-badge          Text for the "Magento" compatibility badge
+     *
+     */
+    public static function getMagentoVersions(): array
+    {
+        return [
+            '2.2' => [
+                'php'               => ['~7.0', '~7.1', '~7.2'],
+                'magento-framework' => ['^101.0.0'],
+            ],
+            '2.3' => [
+                'php'               => ['~7.1', '~7.2', '~7.3'],
+                'magento-framework' => ['^102.0.0'],
+            ],
+            '2.4' => [
+                'php'               => ['~7.3', '~7.4'],
+                'magento-framework' => ['^103.0.0'],
+            ],
+        ];
+    }
+
+    /**
+     * package prefixes that should not be part of the module name (capitalized because used after ucwords())
+     *
+     * This covers e.g.:
+     *
+     *      magento2-
+     *      magento2-module
+     *
+     * @param string $moduleNamespace
+     * @return string[]
+     */
+    private static function getCommonModulePrefixes(string $moduleNamespace): array
+    {
+        return ['Magento2', 'Module', $moduleNamespace];
+    }
+}
